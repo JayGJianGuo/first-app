666f9b07122b23ea1ad1f5879e7a5293448f30f7
Jaykey.Guo <jkland927@gmail.com>
2018-06-09 16:18:03 +0800

    add page and api route

--- route.page.js
+++ route.page.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Get home page. */
 router.get('/', function(req, res, next){
-    res.render('index', {title: "Express"});
+    res.render('index', {title: 'first-app'});
 });
 
 /* Get posts page. */
